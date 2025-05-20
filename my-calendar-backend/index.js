c9d63c0aafd1a72c281364321c9a9e093c2de94d
sanjana-adepu <sanjana.anb.9119@gmail.com>
2025-05-20 15:47:58 +0530

    commited

--- my-calendar-backend/index.js
+++ my-calendar-backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const eventRoutes = require('./routes/eventRoutes');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'http://localhost:5174' }));
 app.use(express.json());
 
 app.get('/', (req, res) => res.json("From Backend side"));
